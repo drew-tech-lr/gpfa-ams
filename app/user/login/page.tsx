a7d89d48dfd78f645b1f7dc81fd3927747c2ef3a
Your Name <andrewlivingstonejr@gmail.com>
2026-03-28 19:54:21 +0000

    setting layouts

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -1,14 +1,38 @@
-import Slider from '@/app/components/slider/Slider'
-import React from 'react'
-import LoginForm from './LoginForm'
+// import Slider from "@/app/components/slider/Slider";
+// import React from "react";
+// import LoginForm from "./LoginForm";
+
+// const loginPage = () => {
+//   return (
+//     <>
+//       <div className="flex">
+//         <LoginForm />
+//         <Slider />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default loginPage;
+
+import Slider from "@/app/components/slider/Slider";
+import React from "react";
+import LoginForm from "./LoginForm";
 
 const loginPage = () => {
   return (
-    <>
-    <LoginForm />
-    {/* <Slider /> */}
-    </>
-  )
-}
+    <div className="flex flex-col md:flex-row min-h-screen">
+      {/* Login Form */}
+      <div className="w-full md:w-1/2">
+        <LoginForm />
+      </div>
 
-export default loginPage
+      {/* Slider */}
+      <div className="hidden md:flex md:w-1/2">
+        <Slider />
+      </div>
+    </div>
+  );
+};
+
+export default loginPage;
